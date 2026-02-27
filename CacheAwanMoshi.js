b1c9bfb433df77a90eadb65581cbb7832dc56376
Ipan <ifanmulyanto59@gmail.com>
2026-02-28 02:22:07 +0800

    Add files via upload

---
+++ CacheAwanMoshi.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "awan-os-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./DASHBOARD_MOSHI AWAN.html",
+        "./Upload.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
