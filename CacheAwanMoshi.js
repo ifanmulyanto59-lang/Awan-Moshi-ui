6acff73c52d781a96cbdbab28d1da66beb9ba758
Ipan <ifanmulyanto59@gmail.com>
2026-02-28 02:31:12 +0800

    Update cache files and change HTML reference

--- CacheAwanMoshi.js
+++ CacheAwanMoshi.js
@@ -5,7 +5,7 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "./",
-        "./DASHBOARD_MOSHI AWAN.html",
+        "./index.html",
         "./Upload.json"
       ]);
     })
@@ -17,4 +17,5 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
+
 });
